8b14db090587686ed4860ac3278e690f24a82c4c
Nickpandiscia <npandiscia1@gmail.com>
2022-06-09 06:52:02 -0400

    initial commit

---
+++ Variables exercise/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+string myName = "Nick";
+int myAge = 26;
+char character = 'N';
+decimal money = 100.5m;
+double city = 5.6;
+bool myCity = true;
+Console.WriteLine($"My name is {myName}, and i'm {myAge} years old.");
+Console.WriteLine($"The first letter of my name starts with {character}.");
+Console.WriteLine($"I live {city} miles from Scranton, PA. I would love to have {money} million dollars.");
+Console.WriteLine($"Yes it is {myCity}. Scranton does exist!");
+
+
